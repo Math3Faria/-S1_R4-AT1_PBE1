ed37355c35b365856bbcee180d651890ea35f83c
Math3Faria <matheusteud12@gmail.com>
2025-09-18 08:59:02 -0300

    atividade2 completa

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 //atividade 02
+const express = require('express');
+const app = express();
+const port = 2600;
+
+app.use(express.json());
+
 app.get('/calculadora', (req, res) => {
   try {
     const { operacao, numUm, numDois } = req.query;
@@ -6,33 +12,33 @@ app.get('/calculadora', (req, res) => {
     const n2 = parseFloat(numDois);
 
     if (isNaN(n1) || isNaN(n2)) {
-      return res.status(400).send('Erro: os dois números devem ser válidos:');
+      res.status(400).send('Erro:Insira dois números devem ser válidos:');
     }
 
     let resultado;
-    let nomeDaOperacao;
 
-    if (nomeDaOperacao == 'soma') {
+    if (operacao === 'soma') {
       resultado = n1 + n2;
-      nomeDaOperacao = 'soma';
-    } else if (nomeDaOperacao == 'subtracao') {
+    } else if (operacao === 'subtracao') {
       resultado = n1 - n2;
-      nomeDaOperacao = 'subtração';
-    } else if (nomeDaOperacao == 'multiplicacao') {
+    } else if (operacao === 'multiplicacao') {
       resultado = n1 * n2;
-      nomeDaOperacao = 'multiplicação';
-    } else if (nomeDaOperacao == 'divisao') {
-      if (n2 == 0) {
-        return res.status(400).send('Erro: Não pode dividir por zero');
+    } else if (operacao === 'divisao') {
+      if (n2 === 0) {
+        res.status(400).send('Erro: Não pode dividir por zero');
       }
       resultado = n1 / n2;
-      nomeDaOperacao = 'divisão';
     } else {
-      return res.status(400).send('Erro: Você escolheu uma forma não valida de operação, Use soma, subtracao, multiplicacao ou divisao.');
+      res.status(400).send('Erro: Você escolheu uma forma não valida de operação, Use soma, subtracao, multiplicacao ou divisao.');
     }
 
-    res.send(`O resultado da: ${nomeDaOperacao} é: ${resultado}`);
+    res.status(200).send(`O resultado da: ${operacao} é: ${resultado}`);
   } catch (error) {
     res.status(500).send('Erro: Falha no servidor.');
   }
+});
+
+
+app.listen(port, () => {
+  console.log(`O server está rodando na porta: ${port}`);
 });
