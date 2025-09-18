030ddef26dd6a6fb90f6f7dd876e9f1be89837dc
Math3Faria <matheusteud12@gmail.com>
2025-09-18 09:22:14 -0300

    ajustando atividade6, deixando pronta e completa

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 //atividade 06
+const express = require('express');
+const app = express();
+const port = 2600;
+
+app.use(express.json());
+
+
 app.get('/imc', (req, res) => {
   try {
     const peso = parseFloat(req.query.peso);
     const altura = parseFloat(req.query.altura);
 
     if (isNaN(peso) || isNaN(altura) || altura <= 0) {
-      return res.status(400).send('Erro: Peso e altura devem ser números válidos e a altura deve ser maior que zero.');
+      return res.status(400).send('Erro: Digite numeros para o peso e altura, e lembre-se que a altura nao pode ser 0');
     }
 
     const imc = peso / (altura * altura);
@@ -21,7 +28,7 @@ app.get('/imc', (req, res) => {
       classificacao = 'Obesidade';
     }
 
-    res.send(`IMC: ${imc.toFixed(2)} - ${classificacao}`);
+    res.status(200).send(`Seu IMC é: ${imc.toFixed(2)}. é considerado um: ${classificacao}`);
   } catch (error) {
     res.status(500).send('Erro: Falha no servidor.');
   }
